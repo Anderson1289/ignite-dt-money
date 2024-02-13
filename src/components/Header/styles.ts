677d8e2864f6efd11b4bbc5cf842fcfbc4953875
Anderson Vieira <andervdcarvalho@hotmail.com>
2024-02-13 00:01:01 -0300

    add styles

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -13,20 +13,22 @@ export const HeaderContent = styled.div`
 
     display: flex;
     justify-content: space-between;
-    align-items: centes;
+    align-items: center;
 `;
 
 export const NewTransactionButton = styled.button`
     height: 50px;
     border: 0;
     background: ${props => props.theme["green-500"]};
-    color: ${props => props.theme.white}
+    color: ${props => props.theme.white};
     font-weight: bold;
     padding: 0 1.25rem;
     border-radius: 6px;
     cursor: pointer;
+    
 
     &:hover {
         background: ${props => props.theme["green-700"]};
+        transition: background-color 0.5s;
     }
 `
